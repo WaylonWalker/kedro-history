f07ce0040309a1ea496fdf33c19e374ac92847b5
Waylon S. Walker <walker_waylon_s@cat.com>
2021-07-09 15:00:00 +0100

    create range_squared node

--- src/kedro_history/pipelines/history_nodes.py
+++ src/kedro_history/pipelines/history_nodes.py
@@ -1,4 +1,7 @@
 from kedro.pipeline import node
 
 
-nodes = [node(lambda: range(1000), None, "range")]
+nodes = [
+    node(lambda: range(1000), None, "range"),
+    node(lambda range: [x ** 2 for x in range], "range", "range_squared"),
+]
